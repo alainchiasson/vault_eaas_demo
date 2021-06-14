6e5980b55ce36ae0d8c6f7a55df13a714e832e3c
Alain Chiasson <alain@chaisson.org>
2021-06-14 08:06:01 -0400

    removed unused files.

--- scripts/rotate_demo.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Rotate 
-vault write -format json transit/encrypt/demo-key plaintext=$(base64 <<< "1111 1111 1111 1111") | jq -r '.data.ciphertext' > test1.txt
-
-# decrypt
-vault write -format json transit/decrypt/demo-key ciphertext=$(cat test1.txt) | jq -r '.data.plaintext' | base64 --decode
